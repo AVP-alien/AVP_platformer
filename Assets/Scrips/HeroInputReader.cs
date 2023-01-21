936ba6e7fc99e60999c38ef06b2eea4b429c2c3a
AVP-alien <122525861+AVP-alien@users.noreply.github.com>
2023-01-21 20:30:48 +0300

    2D-Control-via-Input-Action-Event
    Коррект ДЗ2

--- Assets/Scrips/HeroInputReader.cs
+++ Assets/Scrips/HeroInputReader.cs
@@ -5,24 +5,7 @@ public class HeroInputReader : MonoBehaviour
 {
     [SerializeField] private HERO _hero;
 
-    private HeroInputAction _inputActions;
-
-    //private void Awake()
-    //{
-    //    _inputActions = new HeroInputAction();
-    //   _inputActions.Hero.Horizontal.performed += OnHorizontal;
-    //    _inputActions.Hero.Horizontal.canceled += OnHorizontal;
-    //    _inputActions.Hero.SaySomething.performed += OnSaySomething;
-    //}
-    //private void OnEnable()
-    //{
-    //    _inputActions.Enable();
-    //}
-    //public void OnHorizontal(InputAction.CallbackContext context)
-    //{
-    //    var direction = context.ReadValue<float>();
-    //    _hero.SetDirection(direction);
-    //}
+    
     public void On2DMovement(InputAction.CallbackContext context)
     {
         var direction = context.ReadValue<Vector2>();
