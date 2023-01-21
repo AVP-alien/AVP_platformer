488bc2912f04efc7673cc4cf16b042ec5f34d849
AVP-alien <122525861+AVP-alien@users.noreply.github.com>
2023-01-21 20:35:27 +0300

    HomeWork correct

--- Assets/Scrips/HERO.cs
+++ Assets/Scrips/HERO.cs
@@ -2,8 +2,8 @@
 
 public class HERO : MonoBehaviour
 {
-    [SerializeField]    private float _speedX;
-    [SerializeField]    private float _speedY;
+    [SerializeField]    private float _speed;
+  
     private Vector2 _direction;
 
     public void SetDirection( Vector2 direction)
@@ -12,11 +12,10 @@ public class HERO : MonoBehaviour
     }
     private void Update()
     {
-        if (_speedX != 0 || _speedY!=0 )
+        if (_speed != 0 )
         {
-            var deltax = _direction * _speedX * Time.deltaTime;
-            var deltay = _direction * _speedY * Time.deltaTime;
-            transform.position += new Vector3(deltax.x, deltay.y,transform.position.z);
+           var delta = _direction * _speed * Time.deltaTime;
+            transform.position += new Vector3(delta.x, delta.y,transform.position.z);
         }
     }
     public void SaySomething()
