05f49797070987d97673b0b7da69e386b48940d6
AVP-alien <122525861+AVP-alien@users.noreply.github.com>
2023-02-12 19:07:56 +0300

    Animaton added
    Добавлена анимация

---
+++ Assets/Scrips/Components/SilverCoinsCounterComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+namespace AVPplatformer.Components
+{
+    public class SilverCoinsCounterComponent : MonoBehaviour
+    {
+
+        public int SilverCoinsNum = 0;
+      
+
+        public void SilverCoinsCounter ()
+        {
+            SilverCoinsNum++;
+            Debug.Log("Вы подобрали серебряную монетку!" + SilverCoinsNum);
+        }
+    }
+}
--- Assets/Scrips/HERO.cs
+++ Assets/Scrips/HERO.cs
@@ -1,25 +1,79 @@
-﻿using UnityEngine;
+﻿using AVPplatformer.Components;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class HERO : MonoBehaviour
 {
-    [SerializeField]    private float _speed;
-  
-    private Vector2 _direction;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerCheck _groundCheck;
 
-    public void SetDirection( Vector2 direction)
+    private Rigidbody2D _rigidbody;
+    private Vector2 _direction;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
+
+    private static readonly int IsGroundKey = Animator.StringToHash("isGround");
+    private static readonly int IsRunningKey = Animator.StringToHash("isRunning");
+    private static readonly int VerticalSpeedKey = Animator.StringToHash("verticalSpeed");
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+   
+    public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        if (_speed != 0 )
+       
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+        var isJumping = _direction.y > 0;
+        if (isJumping)
         {
-           var delta = _direction * _speed * Time.deltaTime;
-            transform.position += new Vector3(delta.x, delta.y,transform.position.z);
+            if (isGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+        }
+        _animator.SetFloat(VerticalSpeedKey, _rigidbody.velocity.y);
+        _animator.SetBool(IsGroundKey, _groundCheck.isTouchingLayer);
+        _animator.SetBool(IsRunningKey, _direction.x != 0 );
+
+
+        UpdateSpriteDirection();
+            
+    }
+    private void UpdateSpriteDirection()
+    {
+        if (_direction.x > 0)
+        {
+            _sprite.flipX = false;
+        }
+        else if (_direction.x < 0)
+        {
+            _sprite.flipX = true;
         }
     }
     public void SaySomething()
     {
         Debug.Log("ОРЕТ");
     }
+
+    private bool isGrounded()
+    {
+        return _groundCheck.isTouchingLayer;
+    }
+
 }
